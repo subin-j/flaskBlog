5e53d6d6fb4b50e3ecb73adb4c7e6adb65fc5445
jadyJeong <coldfeet0816@gmail.com>
2021-01-09 13:39:11 +0900

    forms.py + templates/login,register.html added

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+#forms 에서 클래스를 임포트 하지 않으면 app파일이 인식하지 않는다.
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'bc838d483ea15b50860be587d0554b79'
 
 posts = [
     {
@@ -23,3 +28,29 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    # instantiate 'form' from class
+    form = RegistrationForm()
+    # validates then alerts
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!","success")
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form = form)
+
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    # instantiate 'form' from class
+    form = LoginForm()
+    # validates then alerts & redirect
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data =='1234':
+            flash(f"Logged in successfully!","success")
+            return redirect(url_for('home'))
+
+        else: 
+                flash("Login Unsuccessful.","danger")
+    return render_template('login.html', title='Login',form=form)
+
---
+++ forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired,Length, Email, EqualTo 
+import email_validator
+
+#------------------------Register
+class RegistrationForm(FlaskForm):
+    email = StringField(
+        'Email', validators = [DataRequired(), Email()]
+    )    
+    username = StringField(
+        #validates string input username -is not null and set minmax length with DataRequired and Length modules.
+        'Username', validators = [DataRequired(), Length(min=1, max=20)]
+    )
+    password = PasswordField(
+        'Password', validators = [DataRequired()]
+    )        
+    confirm_password = PasswordField(
+        'Confirm Password',validators = [DataRequired(), EqualTo(password)]
+    )
+    submit = SubmitField('Sign Up')
+
+#------------------------Login
+class LoginForm(FlaskForm):
+    email = StringField(
+        'Email', validators = [DataRequired(), Email()]
+    )
+    password =  PasswordField(
+        'Password', validators = [DataRequired()]
+    )
+    remember = BooleanField(
+        'Remember Me'
+        )
+    submit = SubmitField(
+        'Sign in'
+        )
