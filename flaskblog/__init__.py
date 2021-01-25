ca6957e513b5fdeb52200c5a2088469d6a869419
jadyJeong <coldfeet0816@gmail.com>
2021-01-25 17:22:04 +0900

    additional user authentication with login_manager + created empty account template

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 # from flask_login import LonginManager
 
 app = Flask(__name__)
@@ -8,7 +9,10 @@ app.config['SECRET_KEY'] = 'bc838d483ea15b50860be587d0554b79'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-# login_manager = LonginManager(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view ='login'
+login_manager.login_view_messege_category ='info'
 
 # module 'routes'has to be imported after app declaration. (else,circular error)
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,10 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app ,db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
+from flask_login import login_user, current_user, logout_user,login_required
+
+# fyi , routes= views.
 
 #----------sample DB
 posts = [
@@ -53,19 +56,35 @@ def register():
 
 @app.route("/login", methods=['GET','POST'])
 def login():
+    #if already logged in, redirect to home
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     # instantiate 'form' from class
     form = LoginForm()
     # validates then alerts & redirect
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         
+        #login success
         if user and bcrypt.check_password_hash(user.password,form.password.data):
             login_user(user,remember=form.remember.data)
-
-            flash(f"Logged in successfully!","success")
-            return redirect(url_for('home'))
-
+            next_page = requeest.args.get('next')
+            #redirect to next_page if exist, else redirect to 'home'
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        #login fail
         else: 
-            flash("Login Unsuccessful.","danger")
+            flash("Login Unsuccessful. Please check the credentials.","danger")
+    
     return render_template('login.html', title='Login',form=form)
 
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    logout_user()
+    return render_template('account.html', title='Account')
